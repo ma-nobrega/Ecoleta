2dbcdf74439a12cff5a255b4cd06356e9f86548f
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2020-07-15 21:32:56 -0300

    Navegação entre paginas

---
+++ server/src/controllers/ItemsControllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection'
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(items => {
+      return {
+        id: items.id,
+        title: items.title,
+        image_url: `http://localhost:3333/uploads/${items.image}`,
+      };
+    });
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController
